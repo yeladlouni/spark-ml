e98d6a59936ca0df9b659e9eb5cdd182a9b32f28
yassine <yeladlouni@gmail.com>
2021-10-07 16:46:17 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,16 @@
+name := "spark-ml"
+
+version := "0.1"
+
+scalaVersion := "2.12.15"
+
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.1.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "3.1.2"
+
---
+++ src/main/scala/Example.scala
@@ -0,0 +1,52 @@
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.RFormula
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.sql.SparkSession
+
+object Example {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val df = spark
+      .read
+      .option("inferSchema", true)
+      .option("header", true)
+      .csv("data/realestate.csv")
+
+    df.printSchema()
+
+    val Array(train, valid) = df.randomSplit(Array(0.7, 0.3))
+
+    train.show()
+    valid.show()
+
+    val rForm = new RFormula()
+      .setFormula("price ~ rooms")
+
+    val trainDF = rForm.fit(train)
+
+    val fittedRF = rForm.fit(train)
+    val preparedTrain = fittedRF.transform(train)
+
+    val lr = new LinearRegression().setLabelCol("label").setFeaturesCol("features")
+
+    val model = lr.fit(preparedTrain)
+
+    println(model.intercept)
+    println(model.coefficients)
+
+    val test = spark
+      .read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("data/test.csv")
+
+    val preparedTest = fittedRF.transform(test)
+
+    val predictions = model.transform(preparedTest)
+    predictions.show()
+
+
+  }
+}
